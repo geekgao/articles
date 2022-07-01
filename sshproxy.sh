d674566b8f96f83e38f8f0493a0eabbb09df05e3
Gao Chao <geekgao@users.noreply.github.com>
2022-07-01 16:35:49 +0800

    Update sshproxy.sh

--- sshproxy.sh
+++ sshproxy.sh
@@ -2,4 +2,4 @@
 export http_proxy=socks5://127.0.0.1:1081
 export https_proxy=socks5://127.0.0.1:1081
 export no_proxy=0,1,2,3,4,5,6,7,8,9
-ssh -NC -D 1081 -p 22 gao@111.111.111.111
+ssh -fNC -D 1081 -p 22 gao@111.111.111.111
